8074d3e4f59199406acfcc0cde08bed7eab6befb
monokemonoke <kawabataryuhei@gmail.com>
2022-12-14 23:25:56 +0900

    pass test

--- astPrinter_test.go
+++ astPrinter_test.go
@@ -10,23 +10,27 @@ import (
 var update = flag.Bool("update", false, "update golden files")
 
 func TestParse(t *testing.T) {
-	golden := "testdata/a.go.golden"
+	golden := "testdata/a.golden"
+
 	var result bytes.Buffer
 	err := Parser("./testdata/a.go", &result)
 	if err != nil {
 		t.Errorf("want no err but got %v", err)
 	}
 
+	// update フラグが指定されていれば golden ファイルを更新する
 	if *update {
-		os.WriteFile(golden, result.Bytes(), 0777)
+		os.WriteFile(golden, result.Bytes(), 0644)
 	}
 
+	// golden ファイルを読み込む
 	expected, err := os.ReadFile(golden)
 	if err != nil {
 		t.Fatalf("failed to read golden file: %v", err)
 	}
 
-	if bytes.Equal(result.Bytes(), expected) {
-		t.Errorf("golden file not matched")
+	// 結果が golden ファイルと変わらないことを確認
+	if result.String() != string(expected) {
+		t.Errorf("golden file not matched\n")
 	}
 }
